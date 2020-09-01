99e3d3db8ec764ebbcf41ce1b3ee066d9664c3d6
Gonsoo Moon <gonsoomoon@gmail.com>
2020-09-01 13:29:35 +0000

    add coldstart analysis

--- movielens_getting_started/utils.py
+++ movielens_getting_started/utils.py
@@ -18,3 +18,22 @@ user_id_list = [98,160,20, 100]
     return df_t
 
 
+def get_rich_dataset(df_interaction, df_item):
+    '''
+    영화 타이틀, 장르 및 가독성 있는 날짜를 입력하여 제공
+    '''
+    df_r = df_interaction.copy()
+    df_r.drop(['EVENT_TYPE'], axis=1, inplace=True)
+    df_r.drop(['EVENT_VALUE'], axis=1, inplace=True)    
+
+    # Add readable date
+    data_col_val = df_r.TIMESTAMP.apply(lambda x: get_readable_date(int(x)))
+    df_r.insert(len(df_r.columns),column="DATE", value=data_col_val)    
+    # Add item_title, genre using join opeation
+    df_r = df_r.merge(df_item, on='ITEM_ID', how='inner')
+    col_order = ['USER_ID', 'ITEM_ID','TITLE', 'GENRE', 'TIMESTAMP', 'DATE']
+    df_r = df_r[col_order]
+    return df_r
+
+
+
